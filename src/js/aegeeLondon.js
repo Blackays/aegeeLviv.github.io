c7b936a9436bd007e1cfb4d4706b90d68b7b02a9
Blackays <mni01051999@gmail.com>
2019-04-26 12:40:36 +0300

    MainTry

--- src/js/aegeeLondon.js
+++ src/js/aegeeLondon.js
@@ -1,154 +1,78 @@
-
-function showTopBarEntries() {
-	var menuEntries = d.qsa(".top-bar__tab-container:nth-child(n+3)");
-	menuEntries.forEach(function(e, i) {
-		e.classList.add("top-bar__tab-container--in");
-		e.classList.add("top-bar__tab-container--in" + (i + 1));
-	});
-	
-	d.st(function() { moveLine(); }, 500);
-}
-
-
-
-
-
-// https://developers.google.com/youtube/player_parameters
-// http://stackoverflow.com/questions/8869372/how-do-i-automatically-play-a-youtube-video-iframe-api-muted
-// http://stackoverflow.com/questions/20501010/youtube-api-onplayerready-not-firing
-function onYouTubeIframeAPIReady() { // eslint-disable-line no-unused-vars
-	var player = new YT.Player("intro__video", {
-		events: {
-			onReady: function() {
-				player.mute();
-				player.playVideo();
-			},
-			onStateChange: function(e) {
-				if (e.data === YT.PlayerState.PLAYING) {
-					showTopBarEntries();
-					d.gc("intro__video-container").classList.add("intro__video-container--in");
-					
-					// The 'end' and 'loop' YouTube parameters didn't work
-					var t = 0;
-					var id = d.si(function() {
-						player.seekTo(0);
-						if (t===2) {
-							player.pauseVideo();
-							clearInterval(id);
-						}
-						++t;
-					}, 20000);
-				}
-			}
-		}
-	});
-}
-
-
-
-
-
-function isMobile(width) {
-	return window.innerWidth < (width || 810);
-}
-
-
-
-
-
-function setMargin() {
-	return isMobile() ? 80 : 300;
-}
-
-
-
-
-
-function setBodyHeight(delayed) {
-	var height = d.calcClientHeightsSum("section.skrollr-deck") + setMargin()*6;
-	document.body.style.height = height + "px";
-	
-	delayed ? d.st(function() { moveLine(); }, 500) : moveLine();
-}
-
-
-
-
-
-function moveLine(position) {
-	var hash = window.location.hash;
-	if (position === undefined && hash) {
-		switch (hash.slice(1)) {
-			case "intro":		position = 0; break;
-			case "who-we-are":	position = 1; break;
-			case "activities":	position = 2; break;
-			case "the-board":	position = 3; break;
-			case "join-us":		position = 4; break;
-			case "faq":			position = 5; break;
-			case "contact":		position = 6; break;
-			default:			position = 5; break;
-		}
-	} else if (position === undefined) {
-		position = 0;
+function init() { // eslint-disable-line no-unused-vars
+	function isMobile() {
+		return window.innerWidth < 810;
 	}
 	
-	var topBarPositions = d.calcPositionsToViewport(".top-bar__tab");
-	var line = d.gc("top-bar__line");
-	line.style.width = topBarPositions[position].width + "px";
-	line.style.transform = "translate3d(" + topBarPositions[position].left + "px, 0, 0)";
-}
-
-
-
-
-
-function init() { // eslint-disable-line no-unused-vars
+	function getGap() {
+		return isMobile() ? 80 : 300;
+	}
+	
+	function setBodyHeight() {
+		var height = d.calcClientHeightsSum("section.skrollr-deck") + getGap()*6;
+		document.body.style.height = height + "px";
+	}
+	
+	function showTopBarEntries() {
+		var menuEntries = d.qsa(".top-bar__tab-container:nth-child(n+3)");
+		menuEntries.forEach(function(entry, index) {
+			entry.classList.add("top-bar__tab-container--in", "top-bar__tab-container--in" + (index + 1));
+		});
+	}
+	
+	var tabPosition = undefined;
+	var isLineMoving = false;
+	
+	function moveLine(position, noBlocking) {
+		if (!isLineMoving && ((position !== undefined && position !== tabPosition) || position === undefined)) {
+			tabPosition = typeof position === "number" ? position : tabPosition;
+			isLineMoving = noBlocking ? false : d.st(function() { isLineMoving = false; }, 500);
+			var tabs = d.calcPositionsToViewport(".top-bar__tab");
+			var line = d.gc("top-bar__line");
+			line.style.width = tabs[tabPosition].width + "px";
+			line.style.transform = "translate3d(" + tabs[tabPosition].left + "px, 0, 0)";
+		}
+	}
+	
+	
+	
 	d.st(function() { showTopBarEntries(); }, 2500);
 	setBodyHeight();
-	
-	
-	
-	var margin = setMargin();
-	var gap = -margin;
+	moveLine(0, true);
 	
 	
 	
 	// Sets up Skroller
+	var gap = getGap();
+	
 	var offsetFunctions = {
-		get d0() { return d.gi("intro").clientHeight + margin; },
-		get d0g() { return gap + this.d0; },
-		get d1() { return d.gi("who-we-are").clientHeight + margin + this.d0; },
-		get d1g() { return gap + this.d1; },
-		get d2() { return d.gi("activities").clientHeight + margin + this.d1; },
-		get d2g() { return gap + this.d2; },
-		get d3() { return d.gi("the-board").clientHeight + margin + this.d2; },
-		get d3g() { return gap + this.d3; },
-		get d4() { return d.gi("join-us").clientHeight + margin + this.d3; },
-		get d4g() { return gap + this.d4; },
-		get d5() { return d.gi("faq").clientHeight + margin + this.d4; },
-		get d5g() { return gap + this.d5; },
-		get d6() { return d.gi("contact").clientHeight + margin + this.d5; }
+		get d0() { return d.gi("intro").clientHeight; },
+		get d0g() { return this.d0 + gap; },
+		get d1() { return d.gi("who-we-are").clientHeight + this.d0g; },
+		get d1g() { return  this.d1 + gap; },
+		get d2() { return d.gi("activities").clientHeight + this.d1g; },
+		get d2g() { return this.d2 + gap; },
+		get d3() { return d.gi("the-board").clientHeight + this.d2g; },
+		get d3g() { return this.d3 + gap; },
+		get d4() { return d.gi("join-us").clientHeight + this.d3g; },
+		get d4g() { return this.d4 + gap; },
+		get d5() { return d.gi("faq").clientHeight + this.d4g; },
+		get d5g() { return this.d5 + gap; }
 	};
-		
+	
 	var skrollrInstance = skrollr.init({
 		smoothScrolling: false,
 		forceHeight: false,
 		constants: offsetFunctions,
 		mobileCheck: function() { return false; },
 		keyframe: function(element, name, direction) {
-			// console.log("keyframe:");
-			// console.log(name);
-			// console.log(name.slice(6));
-			var extra = 0;
-			if (direction === "up")
-				--extra;
+			var up = direction === "up" ? -1 : 0;
 			switch (name.slice(6)) {
-				case "0g": moveLine(1 + extra); break;
-				case "1g": moveLine(2 + extra); break;
-				case "2g": moveLine(3 + extra); break;
-				case "3g": moveLine(4 + extra); break;
-				case "4g": moveLine(5 + extra); break;
-				case "5g": moveLine(6 + extra);
+				case "0": moveLine(1 + up); break;
+				case "1": moveLine(2 + up); break;
+				case "2": moveLine(3 + up); break;
+				case "3": moveLine(4 + up); break;
+				case "4": moveLine(5 + up); break;
+				case "5": moveLine(6 + up); break;
 			}
 		}
 	});
@@ -159,32 +83,49 @@ function init() { // eslint-disable-line no-unused-vars
 		easing: "outCubic",
 		duration: 500,
 		handleLink: function(link) {
-			var extra = 1;
+			var extraSpace = -100;
 			var linkText = link.href.split("#").pop();
-			
-			try { // To prevent SYNTAX_ERR exception
-				if (d.qs("#" + linkText) === null)
-					throw Error;
-			} catch(e) {
-				document.location.hash = "";
-				return 0;
-			}
+			var linkPositionActivities = d.calcRelativePosition("#activities", "#" + linkText);
+			var linkPositionBoard = d.calcRelativePosition("#the-board", "#" + linkText);
+			var linkPositionFaq = d.calcRelativePosition("#faq", "#" + linkText);
 			
 			switch (linkText) {
-				case "intro":		return 0;
-				case "who-we-are":	return offsetFunctions.d0 + extra;
-				case "activities":	return offsetFunctions.d1 + extra;
-				case "the-board":	return offsetFunctions.d2 + extra;
-				case "join-us":		return offsetFunctions.d3 + extra;
-				case "faq":			return offsetFunctions.d4 + extra;
-				case "contact":		return offsetFunctions.d5 + extra;
+				case "intro":														moveLine(0); return 0;
+				case "who-we-are":													moveLine(1); return offsetFunctions.d0g;
+				case "activities":													moveLine(2); return offsetFunctions.d1g;
+				case "activities-in-london":
+				case "activities-around-europe":
+				case "summer-universities":
+				case "agoras":
+				case "european-planning-meetings":
+				case "network-meetings":
+				case "training-courses":
+				case "many-other-events":											moveLine(2); return offsetFunctions.d1g + linkPositionActivities.top + extraSpace;
+				case "the-board":													moveLine(3); return offsetFunctions.d2g;
+				case "become-member-board":
+				case "former-boards":
+				case "antennas-birth":
+				case "previous-foundations":										moveLine(3); return offsetFunctions.d2g + linkPositionBoard.top + extraSpace;
+				case "join-us":														moveLine(4); return offsetFunctions.d3g;
+				case "faq":															moveLine(5); return offsetFunctions.d4g;
+				case "how-old-can-i-be":
+				case "do-i-need-to-be-university-student":
+				case "can-i-join-aegee-london-even-if-i-dont-live-in-london":
+				case "what-have-to-do-summer-universities-with-real-universities":
+				case "when-do-you-organise-the-next-event":
+				case "could-you-arrange-any-lodging-for-me":
+				case "would-you-like-to-meet":
+				case "which-social-networks-do-you-use":
+				case "is-aegee-london-an-ngo":
+				case "do-you-have-physical-offices":
+				case "are-you-present-at-some-london-university":
+				case "where-does-the-antenna-get-funds-from":
+				case "what-are-the-funds-used-for":
+				case "how-is-this-website-made":									moveLine(5); return offsetFunctions.d4g + linkPositionFaq.top + extraSpace;
+				case "contact":														moveLine(6); return offsetFunctions.d5g;
 			}
 			
-			var linkPosition = d.calcRelativePosition("#faq", "#" + linkText);
-				
-			if (linkPosition)
-				return offsetFunctions.d4 + linkPosition.top - 70;
-			
+			window.history.replaceState(null, null, " "); // https://stackoverflow.com/questions/1397329/how-to-remove-the-hash-from-window-location-url-with-javascript-without-page-r
 			return 0;
 		}
 	});
@@ -194,17 +135,16 @@ function init() { // eslint-disable-line no-unused-vars
 	// Sets the size FB iframes depending on desktop or mobile
 	var width = isMobile() ? 320 : 500,
 		height = isMobile() ? 500 : 560,
-		width2 = isMobile() ? 320 : 500,
 		height2 = isMobile() ? 400 : 500;
 	
 	d.gc("activities__fb-iframe").src = "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FAegeeLondon%2F&tabs=events&small_header=true&hide_cover=false&show_facepile=true&width=" + width + "&height=" + height;
-	d.gc("contact__fb-iframe").src = "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FAegeeLondon%2F&tabs=messages&small_header=false&hide_cover=false&show_facepile=true&width=" + width2 + "&height=" + height2;
+	d.gc("contact__fb-iframe").src = "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FAegeeLondon%2F&tabs=messages&small_header=false&hide_cover=false&show_facepile=true&width=" + width + "&height=" + height2;
 	
 	
 	
 	// Loads the intro video if on desktop
 	if (!isMobile() && (d.getOS() === "Windows" || d.getOS() === "macOS" || d.getOS() === "Linux")) {
-		d.gc("intro__video").src = "//www.youtube.com/embed/7x8BCbo45qA?controls=0&enablejsapi=1&showinfo=0&rel=0&iv_load_policy=3&disablekb=1&origin=http://aegee-london.eu";
+		// d.gc("intro__video").src = "http://www.youtube.com/embed/7x8BCbo45qA?&mute=1&controls=0&enablejsapi=1&showinfo=0&rel=0&iv_load_policy=3&disablekb=1&origin=http://localhost:3000";
 	}
 	
 	
@@ -225,7 +165,11 @@ function init() { // eslint-disable-line no-unused-vars
 	});
 	
 	d.ae("resize", function() {
-		setBodyHeight(true);
+		d.st(function() {
+			moveLine();
+			setBodyHeight();
+			skrollrInstance.refresh();
+		}, 300);
 		d.gc("top-bar").classList.remove("top-bar--open");
 		d.gc("top-bar__three-bars-close-surface").classList.remove("top-bar__three-bars-close-surface--in");
 	});
@@ -240,9 +184,7 @@ function init() { // eslint-disable-line no-unused-vars
 	var hash = window.location.hash;
 	if ((hash && hash !== "#intro") || isMobile()) {
 		d.gc("top-bar").classList.add("top-bar--in");
-		d.gc("top-bar").classList.add("top-bar--in-no-delay");
 		d.st(function() { d.gc("top-bar").classList.remove("top-bar--in-no-delay"); }, 1000);
-		
 		showTopBarEntries();
 	}
 	
@@ -272,4 +214,36 @@ function init() { // eslint-disable-line no-unused-vars
 	
 	
 	d.gc("body").classList.add("body--in");
+}
+
+
+
+// https://developers.google.com/youtube/player_parameters
+// http://stackoverflow.com/questions/8869372/how-do-i-automatically-play-a-youtube-video-iframe-api-muted
+// http://stackoverflow.com/questions/20501010/youtube-api-onplayerready-not-firing
+function onYouTubeIframeAPIReady() { // eslint-disable-line no-unused-vars
+	var player = new YT.Player("intro__video", {
+		events: {
+			onReady: function() {
+				player.mute();
+				player.playVideo();
+			},
+			onStateChange: function(e) {
+				if (e.data === YT.PlayerState.PLAYING) {
+					d.gc("intro__video-container").classList.add("intro__video-container--in");
+					
+					// The 'end' and 'loop' YouTube parameters didn't work
+					var t = 0;
+					var id = d.si(function() {
+						player.seekTo(0);
+						if (t===2) {
+							player.pauseVideo();
+							clearInterval(id);
+						}
+						++t;
+					}, 20000);
+				}
+			}
+		}
+	});
 }
